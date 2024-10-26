4738e5f7531548c9759848da34829e70bc1642b3
Tuxedolphin <zhuzhen.zhuo@gmail.com>
2024-10-26 18:25:51 +0800

    Added Aces Form Submission Page

--- src/Components/AcesForm.tsx
+++ src/Components/AcesForm.tsx
@@ -8,18 +8,21 @@ import {
   MenuItem,
   SelectChangeEvent,
 } from "@mui/material";
-import { FireTruck, WhereToVote } from '@mui/icons-material/';
-import { ReactComponent as FireAlarm} from "../assets/alarm-siren.svg"
+import { FireTruck, WhereToVote } from "@mui/icons-material";
+import FireAlarm from "../assets/alarm-siren.svg";
 import { FC, useState } from "react";
 import { Box } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import { TimePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
+/**
+ * Defining constants for styling
+ */
 const mainGridFormat = {
   container: true,
   spacing: { xs: 2, md: 3 },
-  columns: { xs: 4, sm: 8, md: 12 },
+  columns: { xs: 4, sm: 8 },
 };
 
 const smallInput = {
@@ -34,9 +37,41 @@ const largeInput = {
   md: 8,
 };
 
+const iconInputBox = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-evenly",
+  marginTop: 2,
+};
+
+const TimingInputs: FC = () => {
+  return (
+    <Paper sx={{ p: 1, marginTop: 2 }}>
+      <Divider>ACES Information</Divider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Box sx={{ marginRight: "10vw" }}>
+          <Box sx={iconInputBox}>
+            <img src={FireAlarm} alt="Fire Alarm" width="45" height="45" />
+            <TimePicker label="Time Dispatched" />
+          </Box>
+          <Box sx={iconInputBox}>
+            <FireTruck sx={{ width: 45, height: 45 }} />
+            <TimePicker label="Time Dispatched" />
+          </Box>
+          <Box sx={iconInputBox}>
+            <WhereToVote sx={{ width: 45, height: 45 }} />
+            <TimePicker label="Time Dispatched" />
+          </Box>
+        </Box>
+      </LocalizationProvider>
+    </Paper>
+  );
+};
+
 interface AcesFormProps {
   isLA: boolean;
   opsCenterAcknowledge: boolean;
+  setText: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const AcesForm: FC<AcesFormProps> = (props) => {
@@ -72,16 +107,8 @@ const AcesForm: FC<AcesFormProps> = (props) => {
         </Grid>
         <TextField label="Incident Outcome" multiline rows={3} fullWidth />
       </Paper>
-      <Paper sx={{ p: 1, marginTop: 2 }}>
-        <Divider sx={{ paddingBottom: 1 }}>ACES Information</Divider>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <Box sx={{display: "flex", }}>
-            <TimePicker label="Time Dispatched" />
-            <TimePicker label="Time Dispatched" />
-            <TimePicker label="Time Dispatched" />
-          </Box>
-        </LocalizationProvider>
-      </Paper>
+      <TimingInputs />
+      <TimingInputs />
     </>
   );
 };
--- src/Pages/AddEntryPage.tsx
+++ src/Pages/AddEntryPage.tsx
@@ -1,16 +1,5 @@
-import { useState, FC } from "react";
-import {
-  TextField,
-  Typography,
-  Divider,
-  MobileStepper,
-  FormLabel,
-  Theme,
-  Box,
-  Paper,
-  Button,
-} from "@mui/material";
-import Grid from "@mui/material/Grid2";
+import React, { useState, useEffect, FC } from "react";
+import { MobileStepper, Box, Button } from "@mui/material";
 import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
 
 import "@fontsource/roboto/300.css";
@@ -18,9 +7,38 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-const AddEntryPage: FC = () => {
+import FirstPageForm from "../Components/FirstPageForm";
+import AcesForm from "../Components/AcesForm";
+
+const mainGridFormat = {
+  container: true,
+  spacing: { xs: 2, md: 3 },
+  columns: { xs: 4, sm: 8, md: 12 },
+};
+
+const smallInput = {
+  xs: 2,
+  sm: 4,
+  md: 4,
+};
+
+const largeInput = {
+  xs: 4,
+  sm: 8,
+  md: 8,
+};
+
+const stepsContent = [<FirstPageForm key={0} />, <AcesForm key={1} />];
+
+interface AddEntryPageProps {
+  setText: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const AddEntryPage: FC<AddEntryPageProps> = (props) => {
   const [activeStep, setActiveStep] = useState(0);
-  const maxSteps = 3;
+  const [maxSteps, setMaxSteps] = useState(3);
+
+  props.setText("Add Incident");
 
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -31,55 +49,38 @@ const AddEntryPage: FC = () => {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Paper elevation={1} square={false}>
-        <Paper
-          square
-          elevation={2}
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            height: 75,
-            pl: 2,
-          }}
-        >
-          <Typography variant="h5">General Information</Typography>
-        </Paper>
-        <Box sx={{ height: "78vh", width: "100%", p: 2 }}>
-          <TextField
-            id="incNumber"
-            label="Incident Number"
-            variant="outlined"
-            fullWidth
-          />
-        </Box>
-        <MobileStepper
-          variant="text"
-          steps={maxSteps}
-          position="static"
-          activeStep={activeStep}
-          nextButton={
-            <Button
-              size="small"
-              onClick={handleNext}
-              disabled={activeStep === maxSteps - 1}
-            >
-              Next
-              <KeyboardArrowRight />
-            </Button>
-          }
-          backButton={
-            <Button
-              size="small"
-              onClick={handleBack}
-              disabled={activeStep === 0}
-            >
-              <KeyboardArrowLeft />
-              Back
-            </Button>
-          }
-        />
-      </Paper>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "calc(100vh - 56px)",
+      }}
+    >
+      <Box sx={{ width: "100%", p: 1, flexGrow: 2 }}>
+        {stepsContent[activeStep]}
+      </Box>
+      <MobileStepper
+        variant="progress"
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            Next
+            <KeyboardArrowRight />
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            <KeyboardArrowLeft />
+            Back
+          </Button>
+        }
+      />
     </Box>
   );
 };
