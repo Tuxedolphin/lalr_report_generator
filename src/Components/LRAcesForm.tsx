570f80b15e60df44e153f7121ad289ed40ad1a42
Tuxedolphin <zhuzhen.zhuo@gmail.com>
2024-10-27 13:46:39 +0800

    Added Other Pages Of Form

---
+++ src/Components/LRAcesForm.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { TimingInputs } from "./AcesForm";
+import { Paper } from "@mui/material";
+
+const LRAcesForm: FC = () => {
+  return (
+    <>
+      <Paper></Paper>
+
+      <TimingInputs headerText="Timings From Footage" />
+    </>
+  );
+};
+
+export default LRAcesForm;
