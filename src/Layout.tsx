5b0ea9b03c5e112028ab226190a004b46eec5b21
Tuxedolphin <zhuzhen.zhuo@gmail.com>
2024-10-28 16:39:53 +0800

    Added Basic Layout

---
+++ src/Layout.tsx
@@ -0,0 +1,24 @@
+import NavBar from "./Components/NavBar";
+import { Outlet } from "react-router-dom"
+import { FC } from "react"
+
+interface LayoutProps {
+    isDarkMode: boolean;
+    setIsDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
+    text: string;
+  }
+
+const Layout: FC<LayoutProps> = (props) => {
+
+    return (
+        <>
+            <NavBar {...props} />
+            <main>
+                <Outlet />
+            </main>
+        </>
+    )
+}
+
+export default Layout
+
