bafb360ba36bca5a38a911025e99f848b0ae3a3b
Tuxedolphin <zhuzhen.zhuo@gmail.com>
2024-10-31 00:19:33 +0800

    Added Image Uploading, Cropping And Displaying

--- src/Components/AddPhotosButton.tsx
+++ src/Components/AddPhotosButton.tsx
@@ -1,13 +1,10 @@
-import {
-  styled,
-  Typography,
-  IconButton,
-  Box,
-  Button,
-} from "@mui/material";
+import { styled, Typography, IconButton, Box, Button } from "@mui/material";
 import { FC, useState } from "react";
 import ImageIcon from "@mui/icons-material/Image";
 import EditPhotoModal from "./EditPhotoModal";
+import Canvas from "./Canvas";
+import { Report } from "../Classes/Report";
+import { Crop } from "react-image-crop";
 
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
@@ -22,53 +19,76 @@ const VisuallyHiddenInput = styled("input")({
 });
 
 interface AddPhotosFormProps {
-  uploadPhotoType: string;
+  uploadPhotoText: string;
+  reportEntry: Report;
+  updateEntry: (key: string, value: string) => void;
 }
 
 const AddPhotosButton: FC<AddPhotosFormProps> = (props) => {
   const [image, setImage] = useState<HTMLImageElement>();
 
+  const { uploadPhotoText, reportEntry, updateEntry } = props;
+
+  const [crop, setCrop] = useState<Crop>({
+    unit: "%",
+    x: 0,
+    y: 0,
+    width: 80,
+    height: 60,
+  });
+
+  const [changeButton, setChangeButton] = useState(false); // To set if we need to change the button to the image
+
   return (
     <>
-      <Button
-        component="label"
-        role={undefined}
-        variant="outlined"
-        tabIndex={-1}
-        sx={{
-          width: "100%",
-          aspectRatio: 4 / 3,
-          color: "primary",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <ImageIcon sx={{ width: 50, height: 50 }} />
-          <Typography fontSize={18}>
-            {props.uploadPhotoType.toUpperCase()}
-          </Typography>
-        </Box>
-        <VisuallyHiddenInput
-          type="file"
-          onChange={(event) => {
-            if (event.target.files) {
-              const image = new Image();
-              image.src = URL.createObjectURL(event.target.files[0]);
-              setImage(image);
-            }
-          }}
-        />
-      </Button>
+      <Box sx={{ width: "100%", aspectRatio: 4 / 3 }}>
+        {changeButton && image ? (
+          <Canvas crop={crop} image={image} />
+        ) : (
+          <Button
+            component="label"
+            role={undefined}
+            variant="outlined"
+            tabIndex={-1}
+            sx={{
+              width: "100%",
+              height: "100%",
+              color: "primary",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <ImageIcon sx={{ width: 50, height: 50 }} />
+              <Typography fontSize={18}>
+                {props.uploadPhotoText.toUpperCase()}
+              </Typography>
+            </Box>
+            <VisuallyHiddenInput
+              type="file"
+              onChange={(event) => {
+                if (event.target.files) {
+                  const image = new Image();
+                  image.src = URL.createObjectURL(event.target.files[0]);
+                  setImage(image);
+                }
+              }}
+            />
+          </Button>
+        )}
+      </Box>
       <EditPhotoModal
         image={image}
         setImage={setImage}
-        titleText={"Edit " + props.uploadPhotoType}
+        titleText={"Edit " + props.uploadPhotoText}
+        crop={crop}
+        setCrop={setCrop}
+        setChangeButton={setChangeButton}
       />
     </>
   );
--- src/Components/Canvas.tsx
+++ src/Components/Canvas.tsx
@@ -1,23 +1,48 @@
-import { FC, useEffect, useRef } from "react"
+import { FC, useEffect, useRef } from "react";
 import { type Crop } from "react-image-crop";
 
 interface CanvasProps {
-    image: HTMLImageElement;
-    crop: Crop;
+  image: HTMLImageElement;
+  crop: Crop;
 }
 
 const Canvas: FC<CanvasProps> = (props) => {
-    
-    const ref = useRef<HTMLCanvasElement>(null);
+  const { image, crop } = props;
 
-    useEffect(() => {
-        const canvas = ref.current!;
-        const context = canvas.getContext("2d");
-    },[])
+  const ref = useRef<HTMLCanvasElement>(null);
 
-    return (
-        <canvas ref={ref} />
-    )
-}
+  useEffect(() => {
 
-export default Canvas
+    const canvas = ref.current!;
+    const context = canvas.getContext("2d")!;
+
+    canvas.width = image.naturalWidth * (crop.height / 100);
+    canvas.height = image.naturalHeight * (crop.height / 100);
+
+    const cropX = image.naturalWidth * (crop.x / 100);
+    const cropY = image.naturalHeight * (crop.y / 100);
+
+    context.translate(-cropX, -cropY);
+
+    context.drawImage(
+        image,
+        0,
+        0,
+        image.naturalWidth,
+        image.naturalHeight,
+        0,
+        0,
+        image.naturalWidth,
+        image.naturalHeight,
+    );
+
+  }, [image, crop]);
+
+  return (
+    <>
+      <canvas ref={ref} style={{width: "100%"}}/>
+    </>
+  );
+};
+
+export default Canvas;
