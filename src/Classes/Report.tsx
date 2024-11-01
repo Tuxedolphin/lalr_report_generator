86ee7fd79de0f8b8b4e5f1eedaff7b1ca9232987
Tuxedolphin <zhuzhen.zhuo@gmail.com>
2024-11-02 01:03:32 +0800

    Updated Type And Interfaces

--- src/Classes/Report.tsx
+++ src/Classes/Report.tsx
@@ -1,8 +1,9 @@
+import { Crop } from "react-image-crop";
+import { Dayjs } from "dayjs";
+
 export type reportType = "LA" | "LR" | undefined;
 
-export type ReportValueTypes = File | Blob | Crop | string;
-
-export class reportImage {
+export class ReportImage {
   image: HTMLImageElement = new Image();
   crop: Crop | undefined;
   blob: Blob | undefined;
@@ -10,7 +11,6 @@ export class reportImage {
   constructor();
   constructor(image: File | Blob, crop: Crop);
   constructor(image?: File | Blob, crop?: Crop) {
-    
     if (image) {
       this.image.src = URL.createObjectURL(image);
       this.crop = crop;
@@ -19,7 +19,7 @@ export class reportImage {
 
   updateImage(image: File | Blob): void {
     this.image.src = URL.createObjectURL(image);
-  } 
+  }
 }
 
 export interface incidentInformationType {
@@ -34,40 +34,90 @@ export interface incidentInformationType {
   opsCenterAcknowledged: boolean;
 }
 
-export interface LAInformationType {
+export type LAInformationKeyType =
+  | keyof LAInformationType["generalInformation"]
+  | keyof LAInformationType["acesInformation"]
+  | keyof LAInformationType["cameraInformation"];
 
+export interface LAInformationType {
+  generalInformation: {
+    justification: string;
+  };
+
+  acesInformation: {
+    timeDispatched?: Dayjs;
+    timeResponded?: Dayjs;
+    acesScreenshot?: ReportImage;
+  };
+
+  cameraInformation: {
+    timeDispatched?: Dayjs;
+    timeResponded?: Dayjs;
+    allInPhoto?: ReportImage;
+    moveOffPhoto?: ReportImage;
+  };
 }
 
-export interface LRInformationType {}
+export type LRInformationKeyType =
+  | keyof LRInformationType["generalInformation"]
+  | keyof LRInformationType["acesInformation"]
+  | keyof LRInformationType["cameraInformation"];
+
+export interface LRInformationType {
+  generalInformation: {
+    boundary?: string;
+    justification?: string;
+    weather?: string;
+  };
+
+  acesInformation: {
+    timeDispatched?: Dayjs;
+    timeEnRoute?: Dayjs;
+    acesScreenshot?: ReportImage;
+  };
+
+  cameraInformation: {
+    timeMoveOff?: Dayjs;
+    timeArrived?: Dayjs;
+    bufferingTime?: Dayjs | 0 | null;
+    bufferingLocation?: string;
+    moveOffPhoto?: ReportImage;
+    arrivedPhoto?: ReportImage;
+  };
+}
+
+export type ReportValueKeysType =
+  | keyof incidentInformationType
+  | LAInformationKeyType
+  | LRInformationKeyType;
+export type ReportValueTypes = ReportImage | Dayjs | string | 0 | null;
 
 export class Report {
-  
+  id = -1;
   type: reportType | undefined;
   incidentInformation: incidentInformationType | undefined;
   reportInformation: LAInformationType | LRInformationType | undefined;
 
   constructor();
   constructor(
-    image: File | Blob,
+    id: number,
     type: reportType,
-    crop: Crop,
     incidentInformation: incidentInformationType,
-    reportInformation: LAInformationType | LRInformationType,
+    reportInformation: LAInformationType | LRInformationType
   );
 
   constructor(
-    image?: File | Blob,
+    id?: number,
     type?: reportType,
-    crop?: Crop,
     incidentInformation?: incidentInformationType,
-    reportInformation?: LAInformationType | LRInformationType,
+    reportInformation?: LAInformationType | LRInformationType
   ) {
-    if (image) {
-      this.image.src = URL.createObjectURL(image);
+    if (id) {
+      this.id = id;
       this.type = type;
-      this.crop = crop;
-      this.blob = new Blob([image]);
       this.incidentInformation = incidentInformation;
+
+      //TODO: Add type checking to ensure that the input information is correct (either LA or LR)
       this.reportInformation = reportInformation;
     }
   }
