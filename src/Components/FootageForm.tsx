74fa526d2d1614a5d2dfb2d6fd91043c9d30b0f1
Tuxedolphin <zhuzhen.zhuo@gmail.com>
2024-11-20 23:23:52 +0800

    Added Basic Functionalities To Footage Form

--- src/Components/FootageForm.tsx
+++ src/Components/FootageForm.tsx
@@ -1,8 +1,16 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { TimingInputs, TimingInputsType } from "./TimingInputs";
-import AddPhotosButton from "./AddPhotosButton";
+import TimingAndPhotoInput from "./TimingAndPhotoInput";
 import { Paper } from "@mui/material";
-import { type MultipleInputEditsType, Report } from "../Classes/Report";
+import { Dayjs } from "dayjs";
+import {
+  type MultipleInputEditsType,
+  type GeneralInformationType,
+  type AcesInformationType,
+  type CameraInformationType,
+  Report,
+  ReportImage,
+} from "../Classes/Report";
 
 interface FootageFormProps {
   setText: React.Dispatch<React.SetStateAction<string>>;
@@ -16,35 +24,66 @@ const FootageForm: FC<FootageFormProps> = (props) => {
   const { setText, reportEntry, updateEntry, setActiveStep, isDarkMode } =
     props;
 
-  const [timingInputs, setTimingInputs] = useState<TimingInputsType>(
-    reportEntry.incidentInformation.reportType == "LA"
-      ? {
-          timeDispatched: null,
-          timeResponded: null,
-        }
-      : {
-          timeDispatched: null,
-          timeEnRoute: null,
-          timeArrived: null,
-        }
-  );
+  const isLA = reportEntry.incidentInformation.reportType === "LA";
 
-  const isLA = reportEntry.incidentInformation.reportType == "LA";
+  const [cameraInformation, setCameraInformation] =
+    useState<CameraInformationType>(
+      isLA
+        ? {
+            timeDispatched:
+              reportEntry.cameraInformation.timeDispatched ?? null,
+            timeAllIn: reportEntry.cameraInformation.timeAllIn ?? null,
+            timeMoveOff: reportEntry.cameraInformation.timeMoveOff ?? null,
+          }
+        : {
+            timeResponded: reportEntry.cameraInformation.timeResponded ?? null,
+            timeArrived: reportEntry.cameraInformation.timeArrived ?? null,
+          }
+    );
+
+  const updateInformation = (
+    key:
+      | keyof GeneralInformationType
+      | keyof AcesInformationType
+      | keyof CameraInformationType,
+    value: string | Dayjs | ReportImage
+  ) => {
+    setCameraInformation({ ...cameraInformation, [key]: value });
+  };
+
+  useEffect(() => {
+
+    if (isLA) {
+      setCameraInformation({
+        ...cameraInformation,
+        dispatchPhoto: cameraInformation.dispatchPhoto ?? new ReportImage(),
+        allInPhoto: cameraInformation.allInPhoto ?? new ReportImage(),
+        moveOffPhoto: cameraInformation.moveOffPhoto ?? new ReportImage(),
+      });
+    } else {
+      setCameraInformation({
+        ...cameraInformation,
+        bufferingTime: cameraInformation.bufferingTime ?? null,
+        bufferingLocation: cameraInformation.bufferingLocation ?? "",
+        moveOffPhoto: cameraInformation.moveOffPhoto ?? new ReportImage(),
+        arrivedPhoto: cameraInformation.arrivedPhoto ?? new ReportImage(),
+      });
+    }
+  }, []);
+
+  const dataInputs = [] as const;
 
   return (
     <>
-    <AddPhotosButton
-      uploadPhotoText={isLA ? }
-    />
-
-      <Paper>
-        <TimingInputs
-          headerText="Timings From Footage"
-          isDarkMode={isDarkMode}
-          timingInputs={timingInputs}
-          setTimingInputs={setTimingInputs}
-        />
-      </Paper>
+      <TimingAndPhotoInput
+        timingInput={{
+          [Object.keys(cameraInformation)[0]]:
+            Object.values(cameraInformation)[0],
+        }}
+        image={reportEntry.cameraInformation.arrivedPhoto ?? new ReportImage()}
+        updateInformation={updateInformation}
+        isDarkMode={isDarkMode}
+      />
     </>
   );
 };
