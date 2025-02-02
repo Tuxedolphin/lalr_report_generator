1f71bcf63efe37f5eae67faac29662c54fb2610a
ZZz <zhuzhen.zhuo@gmail.com>
2025-02-02 20:44:49 +0800

    Added Background Provider Context

--- src/app/Pages/Layout.tsx
+++ src/app/Pages/Layout.tsx
@@ -1,20 +1,20 @@
 import NavBar from "../../components/NavBar";
 import { Outlet } from "react-router-dom";
 import { FC } from "react";
+import UpdateBackgroundProvider from "../../context/UpdateBackgroundProvider";
 
 interface LayoutProps {
-  isDarkMode: boolean;
-  setIsDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
   text: string;
-  setHeight: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const Layout: FC<LayoutProps> = (props) => {
   return (
-    <main>
-      <NavBar {...props} />
-      <Outlet />
-    </main>
+    <UpdateBackgroundProvider>
+      <NavBar {...props}></NavBar>
+      <main>
+        <Outlet />
+      </main>
+    </UpdateBackgroundProvider>
   );
 };
 
